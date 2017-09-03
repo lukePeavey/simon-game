a485a9b761d4aa46bec6f99b7ee9f0c7674ca728
Luke Peavey <1williampeavey@gmail.com>
2017-09-03 17:36:09 -0400

    Rebuild game container

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,78 +1,156 @@
-import React, { Component } from 'react'
-import { random, isEqual, sample, range } from 'lodash'
-import delay from '../utils/delay'
-import Game from '../components/game/Game'
+import React, { Component } from 'react';
+import { random, isEqual, sample, range } from 'lodash';
+import delay from '../utils/delay';
+import Game from '../components/game/Game';
 
 const buttons = [
-  { id: 'green', color: '#37cf48' },
-  { id: 'red', color: '#fa2513' },
-  { id: 'blue', color: '#0187e6' },
-  { id: 'yellow', color: '#fef224' }
-]
+  { id: 'green', color: '#0c9e1c', position: { y: 'top', x: 'left' } },
+  { id: 'red', color: '#a91509', position: { y: 'top', x: 'right' } },
+  { id: 'blue', color: '#0a5c96', position: { y: 'bottom', x: 'left' } },
+  { id: 'yellow', color: '#bfa90c', position: { y: 'bottom', x: 'right' } }
+];
 
 class GameContainer extends Component {
-  
   state = {
     currentLevel: 0,
-    currentStep: 0,
-    sequence: null,
+    fullSequence: null,
+    currentSequence: null,
+    userSequence: null,
     sequenceIsPlaying: false,
     buttons: buttons,
-    activeButtonId: 'red',
-    gameStarted: false
+    activeButtonId: null,
+    message: 'welcome'
+  };
+
+  /**
+   * Generate a new random sequence of 21 steps.
+   */
+  generateNewSequence(length = 21) {
+    return range(0, length).map(() => sample(buttons).id);
   }
 
-  getRandomSequence(length) {
-    return range(0, length).map(() => sample(buttons))
-  }
-
-  startSequence(steps = this.state.current) {
-    const sequence = this.getRandomSequence(steps)
-    
-    this.setState({ sequenceIsPlaying: true, sequence })
-    
-    sequence.map((button, index) => {
-      delay(index * 1500).then(() => {
-        this.setState({activeButtonId: button.id})
+  /**
+   * Plays the current sequence of steps that the user will need to repeat
+   * @todo fix timing
+   */
+  playSequence = () => {
+    const { currentSequence, currentLevel } = this.state;
+    currentSequence.map((id, index) => {
+      delay(index * 1000).then(() => {
+        this.setState({ activeButtonId: id, sequenceIsPlaying: true });
         delay(500).then(() => {
           this.setState({
-            sequenceIsPlaying: index !== (steps - 1),
+            sequenceIsPlaying: index < currentLevel - 1,
             activeButtonId: null
-          })
-        })
-      })
-    })
-  }
+          });
+        });
+      });
+    });
+  };
+
+  /**
+   * Starts the next level after the user successfully enters a sequence of
+   * steps. Each level level adds one more step to the sequence.
+   */
+  startNextLevel = (prevLevel = this.state.currentLevel) => {
+    const currentLevel = prevLevel + 1;
+    const fullSequence =
+      prevLevel > 0 ? this.state.fullSequence : this.generateNewSequence();
+    const delayTime = currentLevel === 1 ? 300 : 2000;
+    const currentSequence = fullSequence.slice(0, currentLevel);
+    const userSequence = [];
+    const message = `Level ${currentLevel}`;
+
+    this.setState({
+      message,
+      currentLevel,
+      fullSequence,
+      currentSequence,
+      userSequence
+    });
+    delay(delayTime).then(() => this.playSequence());
+  };
+
+  startGame = () => {
+    this.startNextLevel(0);
+  };
+
   /**
   |--------------------------------------------------
   | Event Handlers
   |--------------------------------------------------
   */
-  handleClick = event => {
-    console.log(event.target)
-    const { type, currentTarget } = event
-    const id = currentTarget.id
-    if (type === 'mousedown') {
-      this.setState({activeButtonId: id})
-    } else {
-      delay(25).then(() => this.setState({activeButtonId: null}))
+
+  /**
+   * The click handler is triggered manually using mousedown and mouseup.
+   * Mousedown/up are used to manage the active state of the buttons.
+   */
+  handleButtonClick = event => {
+    const { currentTarget: button } = event;
+    let { currentSequence, userSequence, currentLevel } = this.state;
+    console.info('BUTTON CLICK', button);
+    const id = button.id;
+
+    if (currentSequence) {
+      userSequence = [...userSequence, id];
+      if (
+        !isEqual(currentSequence.slice(0, userSequence.length), userSequence)
+      ) {
+        console.info('GAME OVER. TRY AGAIN');
+        this.startGame();
+      } else if (userSequence.length === currentSequence.length) {
+        console.info(`GOOD JOB. YOU MADE IT TO LEVEL ${currentLevel + 1}`);
+        this.startNextLevel();
+      } else {
+        console.info(`CORRECT BUTTON`);
+        this.setState({ userSequence, activeButtonId: id });
+      }
     }
-  }
+  };
 
-  startGame = () => {
-    this.startSequence(10)
-  }
+  /**
+   * Sets the active state of the clicked button, this will cause the button
+   * to light up and the associated audio tone to play while the button is
+   * being pressed.
+   */
+  handleMouseDown = event => {
+    const { button: mouseButton, currentTarget: button } = event;
+    const isPrimaryClick = mouseButton === 0;
+    // Only proceed if this is a primary click/tap event (incomplete solution)
+    if (isPrimaryClick) {
+      console.info('BUTTON CLICK START', button);
+      this.setState({ activeButtonId: button.id });
+    }
+  };
 
+  /**
+   * Removes active state, triggers the "click" handler if mousedown/up
+   * fire on the same button.
+   */
+  handleMouseUp = event => {
+    const { type, currentTarget } = event;
+    const { activeButtonId } = this.state;
+    console.info('BUTTON CLICK END', currentTarget);
+    if (currentTarget.id && currentTarget.id === activeButtonId) {
+      this.handleButtonClick(event);
+    }
+    // Removes active state
+    delay(10).then(() => this.setState({ activeButtonId: null }));
+  };
+
+  /**
+   * Renders the Game component, passing along state and event handlers.
+   */
   render() {
     return (
       <Game
-        activeButtonId={this.state.activeButtonId}
-        buttons={this.state.buttons}
-        handleClick={this.handleClick}
+        handleMouseUp={this.handleMouseUp}
+        handleMouseDown={this.handleMouseDown}
         startGame={this.startGame}
+        {...this.state}
       />
-    )
+    );
   }
 }
 
-export default GameContainer
+export default GameContainer;
