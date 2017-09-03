c1da8aead58c1517ffef9bde462f4c147b7db0e0
Luke Peavey <1williampeavey@gmail.com>
2017-09-03 17:38:53 -0400

    Update Game component

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,26 +1,54 @@
-import React from 'react'
-import tinycolor from 'tinycolor2'
-import Button from '../button/Button'
-import Controls from '../controls/Controls'
-import './Game.css'
+import React from 'react';
+import tinycolor from 'tinycolor2';
+import Buttons from '../buttons/Buttons';
+import CenterPanel from '../center-panel/CenterPanel';
+import styled from 'glamorous';
+import * as globals from '../../styles/global';
 
-const Game = ({startGame, ...rest}) => (
-  <div className="container" onMouseUp={rest.handleClick}>
-    <div className="Game">
-    <Buttons {...rest} />
-    <Controls />
-    </div>
-  </div>
-)
-
-const Buttons = ({buttons, ...rest}) => (
-  <div className="buttons">
-    {buttons.map((props, index) =>
-      <Button {...props} {...rest} key={props.id}
+const Game = ({
+  startGame,
+  handleMouseDown,
+  handleMouseUp,
+  currentLevel,
+  ...rest
+}) =>
+  <Container>
+    <GameInterface id="game">
+      <Buttons
+        handleMouseDown={handleMouseDown}
+        handleMouseUp={handleMouseUp}
+        {...rest}
       />
-    )}
-  </div>
-)
-const styles = {
-}
-export default Game
+      <CenterPanel
+        id="centerPanel"
+        startGame={startGame}
+        message={rest.message}
+      />
+    </GameInterface>
+  </Container>;
+
+/**
+|--------------------------------------------------
+| Style Components
+|--------------------------------------------------
+*/
+const Container = styled.div({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh'
+});
+
+const GameInterface = styled.div({
+  position: 'relative',
+  width: '90vmin',
+  height: '90vmin',
+  maxWidth: '900px',
+
+  borderRadius: '50%',
+  backgroundColor: globals.bgColor,
+  boxShadow: `inset -4px -5px 1px 0px rgba(255, 255, 255, 0.1)`
+});
+
+export default Game;
